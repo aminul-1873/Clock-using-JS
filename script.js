a816b733a8d3d2b88b517cba3b38fdd60d4f408f
aminul-1873 <alfaz1873@gmail.com>
2024-05-21 09:58:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// setInterval(() => {
-//     d = new Date();
-//     htime = d.getHours();
-//     mtime = d.getMinutes();
-//     stime = d.getSeconds();
-//     hrotation = 30*htime + mtime/2;
-//     mrotation = 6*mtime;
-//     srotation = 6*stime;
-//     getElementsByClassName('hour').style.transform = `rotate(${hrotation}deg)`
-//     getElementsByClassName('minute').style.transform = `rotate(${mrotation}deg)`
-//     getElementsByClassName('second').style.transform = `rotate(${srotation}deg)`
-// },1000);
-
 setInterval(() => {
     d = new Date();
     htime = d.getHours();
